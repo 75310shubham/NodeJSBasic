16eeeaa6a577c059817e0b857a0bf81ce91bb0c8
75310shubham <118808458+75310shubham@users.noreply.github.com>
2023-01-10 14:09:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+// const userName="Modi Sahab"
+// console.log(userName);
+
+// const http= require('http');
+// const server = http.createServer();
+// server.listen(3000);
+
+// const http = require("http");
+// function handleRequest(request, response) {
+//   // response.statusCode = 200;
+//   // response.end("<h1>Ek baar phir, modi sarkaar</h1>");
+//   if (request.url === "/currenttime") {
+//     response.statusCode = 200;
+//     response.end("<h1>"+  new Date().toISOString() +"</h1>");
+//   } else if (request.url === "/") response.statusCode = 200;
+//   response.end("<h1>Ek baar phir, modi sarkaar</h1>");
+// }
+// const server = http.createServer(handleRequest);
+// server.listen(3000);
